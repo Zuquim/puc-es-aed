6e9d2148463d78cf17148dfc095104c7173e5650
Marco Tulio Zuquim <mtulio.z@gmail.com>
2017-09-10 13:42:31 -0300

    Starting List_05

---
+++ List_05/src/LinkedListNode.java
@@ -0,0 +1,85 @@
+
+public class LinkedListNode {
+
+	private int id;
+	private LinkedListNode next;
+
+	public LinkedListNode() {
+		this.id = 0;
+		this.next = null;
+	}
+
+	public boolean emptyList() {
+		return (this.getNext() == null);
+	}
+
+	public boolean insert(LinkedListNode node, int n) {
+		boolean success = false;
+		if (this.getNext() == null || n == 1) {
+			LinkedListNode aux = getNext();
+			this.setNext(node);
+			node.setNext(aux);
+			success = true;
+		} else {
+			n--;
+			next.insert(node, n);
+		}
+		return success;
+	}
+
+	public boolean remove(int n) {
+		boolean success = false;
+		if (n == 1 || this.getNext().getNext() == null) {
+			setNext(getNext().getNext());
+		} else {
+			n--;
+			getNext().remove(n);
+		}
+		return success;
+	}
+
+	public void makeEmptyList() {
+		this.setNext(null);
+	}
+
+	public void write() {
+		if (getNext() != null) {
+			System.out.println(getNext().getId());
+			getNext().write();
+		}
+	}
+
+	public int searchPos(int p) {
+		if (p == 1) {
+			return getNext().getId();
+		} else {
+			p--;
+			return getNext().searchPos(p);
+		}
+	}
+
+	public int searchId(int id) {
+		if (getNext().getId() == id) {
+			return getNext().getId();
+		} else {
+			return getNext().getNext().searchId(id);
+		}
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public LinkedListNode getNext() {
+		return next;
+	}
+
+	public void setNext(LinkedListNode next) {
+		this.next = next;
+	}
+
+}
